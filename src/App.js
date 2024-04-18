673d58eb7cf1d1b0d5b3a8ef5def9ad2201193a5
Nico Krause <post@nicokrause.com>
2024-04-18 17:48:15 +0500

    libp2p

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { createHelia } from 'helia'
 import store from './PlaylistsStore'
 import Playlists from './Playlists'
 import Playlist from './Playlist'
-import {config} from './config'
 import './styles/index.scss'
 
 class App extends Component {
@@ -20,7 +19,16 @@ class App extends Component {
         console.log("component mounted")
         let blockstore = new LevelBlockstore("./helia-blocks")
         let datastore = new LevelDatastore("./helia-data")
-        const libp2p = await createLibp2p()
+        const libp2p = await createLibp2p({
+            bootstrap({
+                   list: [
+                       '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+                       '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+                       '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+                       '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+                       '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+                   ]
+                                                           })})
         const ipfs = await createHelia({blockstore, datastore, libp2p})
         await store.connect(ipfs)
         window.store = store
